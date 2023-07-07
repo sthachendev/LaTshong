eabf85c07667b41c9ede50aab3a284f1af960fc0
sthachendev <sthachendev@gmail.com>
2023-07-08 04:16:00 +0600

    +

--- frontend/screens/fn.js
+++ frontend/screens/fn.js
@@ -19,3 +19,31 @@ export const isTokenValid = (token) => {
     return false;
   }
 };
+
+export function capitalizeWords(str) {
+  return str.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(' ');
+}
+
+export function getTimeDifference(postDate) {
+  const date = new Date(postDate);
+  const now = new Date();
+  const diffInSeconds = Math.floor((now - date) / 1000);
+
+  if (diffInSeconds < 60) {
+    return `${diffInSeconds} s ago`;
+  }
+
+  const diffInMinutes = Math.floor(diffInSeconds / 60);
+  if (diffInMinutes < 60) {
+    return `${diffInMinutes} min ago`;
+  }
+
+  const diffInHours = Math.floor(diffInMinutes / 60);
+  if (diffInHours < 24) {
+    return `${diffInHours} h ago`;
+  }
+
+  const diffInDays = Math.floor(diffInHours / 24);
+  return `${diffInDays} d ago`;
+}
+
