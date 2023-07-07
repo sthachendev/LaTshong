bbf2d1e0b2c158f18c85777d980952aceacfb79c
sthachendev <sthachendev@gmail.com>
2023-07-08 04:40:56 +0600

    + safe area view

--- frontend/App.js
+++ frontend/App.js
@@ -1,23 +1,12 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
 import { Provider } from 'react-redux';
-import { SafeAreaView } from 'react-native-safe-area-context';
 import store from './store';
 import Stack from './routes/stack';
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <Provider store={store}>
-        <Stack />
-      </Provider>
-    </SafeAreaView>
+    //this is redux provider
+    <Provider store={store}>
+      <Stack/>
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    // backgroundColor: '#4942E4',
-  },
-});
