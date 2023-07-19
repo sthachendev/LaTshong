da62c237ceb87c3f612106863ceaf202d9251f7f
sthachendev <sthachendev@gmail.com>
2023-07-19 21:58:34 +0600

    postgresql config

---
+++ backend/db.js
@@ -0,0 +1,21 @@
+const { Pool } = require('pg');
+
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const pool = new Pool({
+  // user: process.env.USER,
+  // host: process.env.HOST,
+  // database: process.env.DATABASE,
+  // password: process.env.PASSWORD,
+  // port: process.env.PORT,
+
+  // production
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false // Add this line if you encounter SSL certificate validation errors
+  }
+});
+
+module.exports = pool;
