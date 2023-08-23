74f21f9ed11ec5e74fd04664b705f0d85862ee32
sthachendev <sthachendev@gmail.com>
2023-08-23 20:32:55 +0600

    update validate input fn

--- frontend/screens/fn.js
+++ frontend/screens/fn.js
@@ -1,24 +1,4 @@
 //custom fn
-import jwtDecode from 'jwt-decode';
-
-// export const isTokenValid = (token) => {
-//   try {
-//     const decodedToken = jwtDecode(token);
-//     const currentTime = Date.now() / 1000; // Convert to seconds
-
-//     // Check if the token has an expiration time (exp) and if it's still valid
-//     if (decodedToken.exp && decodedToken.exp > currentTime) {
-//       // Token is valid
-//       return true;
-//     } else {
-//       // Token has expired
-//       return false;
-//     }
-//   } catch (error) {
-//     // If there's an error decoding the token, it's not valid
-//     return false;
-//   }
-// };
 
 export function capitalizeWords(str) {
   return str.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(' ');
@@ -149,13 +129,13 @@ export function validateInput(input) {
     return false;
   }
 
-  var pattern = /^[a-zA-Z]+(?: [a-zA-Z]+)?$/;
+  var pattern = /^[a-zA-Z\s]+$/;
   if (pattern.test(input)) {
     // input is valid
     console.log('valid');
     return true;
   } else {
-    // input contains special characters
+    // input contains special characters or spaces
     console.log('invalid');
     return false;
   }
