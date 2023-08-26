bf96814afe399d1691cfd3488634b86a47d1230a
sthachendev <sthachendev@gmail.com>
2023-08-26 20:45:23 +0600

    Update config.js

--- frontend/screens/config.js
+++ frontend/screens/config.js
@@ -1,9 +1,6 @@
-//api url
-//set the ip of the local machine instead of local host, use ipconfig to get ip
 const config = {
-  // API_URL: "https://latshong.onrender.com",
-  API_URL: "http://10.9.8.140:3000",
+  // API_URL: "https://latshong.onrender.com", example
+  API_URL: "http://10.9.82.81:3000", //web server domain
 };
   
-  export default config;
-  
+export default config;
